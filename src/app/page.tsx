7e6a6edbb61c0c7b50c64dd469586e89486be025
Ahmed <medosamsam919@gmail.com>
2026-01-31 13:39:41 +0200

    fix bug in layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import Footer from "@/components/layout/Footer";
+import Navbar from "@/components/layout/Navbar";
 import ExploreCategories from "@/components/sections/ExploreCategories";
 import FeaturedArticles from "@/components/sections/FeaturedArticles";
 import MeetOurAuthors from "@/components/sections/MeetOurAuthors";
@@ -6,13 +8,15 @@ import SectionWrapper from "@/components/Ui/SectionWrapper";
 
 export default async function Home() {
   return (
-    <div>
+    <>
+      <Navbar />
       <SectionWrapper>
         <FeaturedArticles />
         <ExploreCategories />
         <MeetOurAuthors />
         <NewsletterSubscribe />
       </SectionWrapper>
-    </div>
+      <Footer />
+    </>
   );
 }
