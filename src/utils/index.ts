b0064dec0af52c1006867526399e70fa3448efdb
Ahmed <medosamsam919@gmail.com>
2026-02-02 21:28:38 +0200

    remove unused function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,12 +1,3 @@
-import { blogs } from "@/data/blogs";
-import { IBlog } from "@/types";
-
 export function truncateText(text: string) {
   return text.length > 40 ? text.slice(0, 40) + "..." : text;
 }
-
-export async function getProduct(id: string) {
-  return blogs.find((blog: IBlog) => blog.id === id);
-}
-
-
