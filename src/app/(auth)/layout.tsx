b43b8a300615aab9067c3da281618e5e234e06dc
Ahmed <medosamsam919@gmail.com>
2026-02-07 15:41:03 +0200

    unUsed wrpper for now

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,8 +1,7 @@
 "use client";
-import { SessionProvider } from "next-auth/react";
 import { ReactNode } from "react";
 
 const AuthLayout = ({ children }: { children: ReactNode }) => {
-  return <SessionProvider>{children}</SessionProvider>;
+  return <>{children}</>;
 };
 export default AuthLayout;
