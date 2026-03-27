5d186d48dd99c552ebf8e6b82a45d5d85aac5f81
Ahmed <medosamsam919@gmail.com>
2026-03-27 11:37:26 +0200

    change update-password route to check if new pass is the same as new one

--- src/app/api/user/update-password/route.ts
+++ src/app/api/user/update-password/route.ts
@@ -1,34 +1,51 @@
 import { prisma } from "@/prisma/prisma";
 import { NextRequest, NextResponse } from "next/server";
 import bcrypt from "bcryptjs";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../auth/[...nextauth]/route";
 
 export async function POST(req: NextRequest) {
+  const session = await getServerSession(authOptions)
+  // if (!session || !session.user || !session.user.id) {
+  //   return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+  // }
+  const userId = session?.user.id
+  
   try {
     const body = await req.json();
-    const { id, oldPassword, newPassword } = body;
+    const { oldPassword, newPassword } = body;
 
-    if (!id || !oldPassword || !newPassword) {
+    // Validate required fields
+    if ( !oldPassword || !newPassword) {
       return NextResponse.json({ message: "Missing required fields" }, { status: 400 });
     }
 
     const user = await prisma.user.findUnique({
-      where: { id },
+      where: { id : userId },
     });
 
+    // Check if user exist
     if (!user || !user.password) {
       return NextResponse.json({ message: "User not found or using OAuth" }, { status: 404 });
     }
 
+    // Check if current password is correct
     const isMatch = await bcrypt.compare(oldPassword, user.password);
 
     if (!isMatch) {
       return NextResponse.json({ message: "Incorrect current password" }, { status: 400 });
     }
 
+    // Check if new password is same as old password
+    const isSameAsOldPassword  = await bcrypt.compare(newPassword, user.password);
+
+    if(isSameAsOldPassword ) return NextResponse.json({ message: "New password must be different from current password" }, { status: 400 });
+
     const hashedPassword = await bcrypt.hash(newPassword, 10);
 
+    // Update password
     await prisma.user.update({
-      where: { id },
+      where: { id : userId },
       data: { password: hashedPassword },
     });
 
